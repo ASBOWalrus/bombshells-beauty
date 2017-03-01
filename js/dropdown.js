18f2e4d226b4e5a520f38a7b76c847df55fcb92b
ASBOWalrus <joemarvin95@gmail.com>
2017-03-01 17:51:27 +0000

    Tweaked photo gallery slightly (not finished yet), styled header, created js for dropdown more button

---
+++ js/dropdown.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  var toggle = false;
+  $(".dropdown-btn").on("click", function () {
+    if (toggle === false)
+    {
+      $(this).attr("id", "toggle-on");
+      toggle = true;
+    }
+    else
+    {
+      $(this).attr("id", "");
+      toggle = false;
+    }
+  });
+
+  $(document).on("click", function (e) {
+    if(!$(e.target).is(".btn-group"))
+    {
+      if(toggle === true)
+      {
+        $("#toggle-on").attr("id", "");
+        toggle = false;
+      }
+    }
+  });
+});
