e5b4b455e3bc379ec4fb833163fdcd72e05d83d9
ASBOWalrus <joemarvin95@gmail.com>
2017-03-03 10:43:45 +0000

    Jumbotron parallax jolting fixed, could be slightly jerky if scrolling too quickly

--- js/jumbotron-parallax.js
+++ js/jumbotron-parallax.js
@@ -1,50 +1,67 @@
-/**
- * Author: Heather Corey
- * jQuery Simple Parallax Plugin
- *
- */
+///**
+// * Author: Heather Corey
+// * jQuery Simple Parallax Plugin
+// *
+// */
 
-(function ($) {
+//(function ($) {
 
-  $.fn.parallax = function (options) {
+//  $.fn.parallax = function (options) {
 
-    var windowHeight = $(window).height();
+//    var windowHeight = $(window).height();
 
-    // Establish default settings
-    var settings = $.extend({
-      speed: 0.15
-    }, options);
+//    // Establish default settings
+//    var settings = $.extend({
+//      speed: 0.15
+//    }, options);
 
-    // Iterate over each object in collection
-    return this.each(function () {
+//    // Iterate over each object in collection
+//    return this.each(function () {
 
-      // Save a reference to the element
-      var $this = $(this);
+//      // Save a reference to the element
+//      var $this = $(this);
 
-      // Set up Scroll Handler
-      $(document).scroll(function () {
+//      // Set up Scroll Handler
+//      $(document).scroll(function () {
 
-        var scrollTop = $(window).scrollTop();
-        var offset = $this.offset().top;
-        var height = $this.outerHeight();
+//        var scrollTop = $(window).scrollTop();
+//        var offset = $this.offset().top;
+//        var height = $this.outerHeight();
 
-        // Check if above or below viewport
-        if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
-          return;
-        }
+//        // Check if above or below viewport
+//        if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
+//          return;
+//        }
 
-        var yBgPosition = Math.round((offset - scrollTop) * settings.speed);
+//        var yBgPosition = Math.round((offset - scrollTop) * settings.speed);
 
-        // Apply the Y Background Position to Set the Parallax Effect
-        $this.css('background-position', 'center ' + yBgPosition + 'px');
+//        // Apply the Y Background Position to Set the Parallax Effect
+//        $this.css('background-position', 'center ' + yBgPosition + 'px');
 
-      });
-    });
-  }
-}(jQuery));
+//      });
+//    });
+//  }
+//}(jQuery));
 
-$(document).ready(function () {
-  $('.jumbotron').parallax({
-    speed: 0.4
-  });
+//$(document).ready(function () {
+//  $('.jumbotron').parallax({
+//    speed: 0.4
+//  });
+//});
+
+
+//var jumboHeight = $('.jumbotron').outerHeight();
+
+//function parallax() {
+//  var scrolled = $(window).scrollTop();
+//  $('.bg').css('height', Math.round(jumboHeight - scrolled) + 'px');
+//}
+
+//$(window).scroll(function (e) {
+//  parallax();
+//});
+
+
+$(window).scroll(function () {
+  $(".jumbotron").css("background-position", "50% " + ($(this).scrollTop() / 2) + "px");
 });
