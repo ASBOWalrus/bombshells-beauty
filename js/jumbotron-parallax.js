09086aecde7cd5e7a16e303167724c65c15f2e8b
ASBOWalrus <joemarvin95@gmail.com>
2017-03-14 11:38:11 +0000

    Changed jumbotron parallax to only apply on medium or larger screens

--- js/jumbotron-parallax.js
+++ js/jumbotron-parallax.js
@@ -1,10 +1,14 @@
 $(document).ready(function () {
 	var speed = 0.5;
+	var minWidth = 991;
 
-  	$(window).scroll(function () {
-    	$(".jumbotron").css("-webkit-background-position", "50% " + (($(this).scrollTop() / 2) * speed) + "px");
-    	$(".jumbotron").css("-moz-background-position", "50% " + (($(this).scrollTop() / 2) * speed) + "px");
-    	$(".jumbotron").css("-o-background-position", "50% " + (($(this).scrollTop() / 2) * speed) + "px");
-    	$(".jumbotron").css("background-position", "50% " + (($(this).scrollTop() / 2) * speed) + "px");
-  	});
+	if($(window).width() >= minWidth)
+	{
+		$(window).scroll(function () {
+    		$(".jumbotron").css("-webkit-background-position", "50% " + (($(this).scrollTop() / 2) * speed) + "px");
+    		$(".jumbotron").css("-moz-background-position", "50% " + (($(this).scrollTop() / 2) * speed) + "px");
+    		$(".jumbotron").css("-o-background-position", "50% " + (($(this).scrollTop() / 2) * speed) + "px");
+    		$(".jumbotron").css("background-position", "50% " + (($(this).scrollTop() / 2) * speed) + "px");
+  		});
+	}
 });
